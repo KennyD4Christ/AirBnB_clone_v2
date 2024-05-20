661df41509319c478bc1db229b53db8cca8d0600
KennyD4Christ <kennyd4christ@gmail.com>
2024-05-20 15:01:11 +0100

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,13 +3,13 @@
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
+from models import State
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def closedb(argument):
     """close the db connection"""
 
     storage.close()
@@ -19,10 +19,11 @@ def teardown_appcontext(exception):
 def states_list():
     """/states_list route"""
 
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    states = list(storage.all(State).values())
+    states.sort(key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    storage.reload()
+    app.run("0.0.0.0", 5000)
