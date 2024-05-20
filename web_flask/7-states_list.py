c135364f6cc46dd119b4975d90a3fb3887b5f9d3
KennyD4Christ <kennyd4christ@gmail.com>
2024-05-20 09:30:17 +0100

    web_flask/7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """
-Script to start a Flask web application
+This script initializes and runs a Flask web application that serves a
+list of states retrieved from storage. The application has one route,
+'/states_list', which renders an HTML template displaying the states.
+
+It includes the following functionalities:
+- Initialize the Flask application.
+- Define a teardown function to close the storage session.
+- Define a route to display the list of states sorted by name.
 """
 from flask import Flask, render_template
 from models import storage
@@ -9,22 +16,37 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """
-    Display a list of states
-    """
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
-
-
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """
-    Remove the current SQLAlchemy Session
+    Teardown the app context and close the storage session.
+
+    This function is automatically called after each request to ensure that
+    the current SQLAlchemy session is properly removed, preventing potential
+    issues with database connections.
+
+    :param exception: An exception instance if
+    one was raised during the request
     """
+
     storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    Display a list of states sorted by name.
+
+    This route retrieves all State objects from storage, sorts them by their
+    name attribute, and renders the '7-states_list.html' template to display
+    the sorted list of states.
+
+    :return: Rendered HTML template displaying the sorted list of states.
+    """
+
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 if __name__ == '__main__':
